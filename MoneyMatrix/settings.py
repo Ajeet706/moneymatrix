20c8bc7e7b064885717d71bfeccc9e0196e010cd
Ajeet706 <148783915+Ajeet706@users.noreply.github.com>
2025-10-09 23:02:44 +0530

    Update settings.py
    host name update

--- MoneyMatrix/settings.py
+++ MoneyMatrix/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w&v2hl*h+l@#+w9t_a01r$h00-x=l3jru9n=@nulm930=dxqn2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
